5f5d76f44a26ea5ade154b1d794c0f2212eb1b28
piotr102k <piotrswiszcz2002@wp.pl>
2025-03-12 23:44:34 +0100

    Create initial get endpoint

---
+++ src/app/api/[userId]/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, { params }: { params: { userId: string } }) {
+
+
+
+    return NextResponse.json({
+        id: parseInt(params.userId)
+    })
+
+
+
+
+}
