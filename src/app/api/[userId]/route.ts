1c517440b9c6af3821d15794cb098992847b034a
piotr102k <piotrswiszcz2002@wp.pl>
2025-03-16 12:30:57 +0100

    Fix get endpoint

--- src/app/api/[userId]/route.ts
+++ src/app/api/[userId]/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  { params }: { params: { userId: string } }
+  { params }: { params: Promise<{ userId: string }> }
 ) {
   return NextResponse.json({
-    id: parseInt(params.userId),
+    id: parseInt((await params).userId),
   });
 }
