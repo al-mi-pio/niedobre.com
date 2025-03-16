5c7ddf9b26a95defb0deccc91518c26a936ddc05
piotr102k <piotrswiszcz2002@wp.pl>
2025-03-16 15:11:15 +0100

    Fix lint2

--- src/app/api/[userId]/route.ts
+++ src/app/api/[userId]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server'
 
 export async function GET(
     request: Request,
@@ -6,5 +6,5 @@ export async function GET(
 ) {
     return NextResponse.json({
         id: parseInt((await params).userId),
-    });
+    })
 }
