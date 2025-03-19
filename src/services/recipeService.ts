99ae716239c695a031a9821c3774bcc1a0574630
piotr102k <piotrswiszcz2002@wp.pl>
2025-03-19 23:33:08 +0100

    Add recipy service

---
+++ src/services/recipeService.ts
@@ -0,0 +1,135 @@
+'use server'
+import { Session } from '@/types/Auth'
+
+import { CreateRecipeDTO, Recipe, PatchRecipeDTO, GetRecipeDTO } from '@/types/Recipe'
+import { verifySession } from '@/utils/auth'
+import { getFromFile, setToFile } from '@/utils/file'
+import { randomUUID, UUID } from 'crypto'
+import { join } from 'path'
+import { getIngredientById } from './ingredientService'
+
+export const createRecipe = async (
+    { name, description, instructions, picture, ingredients, cost }: CreateRecipeDTO,
+    session: Session
+) => {
+    const recipeId = randomUUID()
+    const filePath = join(
+        process.cwd(),
+        'src',
+        'data',
+        'users',
+        session.login,
+        'recipes.json'
+    )
+
+    const recipe: Recipe = {
+        id: recipeId,
+        name,
+        description,
+        instructions,
+        picture,
+        ingredients,
+        cost,
+        publicResources: [],
+    }
+    const recipes: Recipe[] = await getCompressedRecipes(session)
+    const newRecipes = [...recipes, recipe]
+
+    await setToFile(filePath, newRecipes)
+}
+
+export const getCompressedRecipes = async (session: Session) => {
+    const filePath = join(
+        process.cwd(),
+        'src',
+        'data',
+        'users',
+        session.login,
+        'recipes.json'
+    )
+
+    const verification = await verifySession(session)
+    if (verification) {
+        const recipes: Recipe[] = await getFromFile(filePath)
+        return recipes
+    }
+    throw new Error('Session is invalid')
+}
+
+export const getRecipes = async (session: Session) => {
+    const compressedRecipes: Recipe[] = await getCompressedRecipes(session)
+    const recipes: GetRecipeDTO[] = await Promise.all(
+        compressedRecipes.map(async (recipe) => ({
+            id: recipe.id,
+            name: recipe.name,
+            description: recipe.description,
+            instructions: recipe.instructions,
+            picture: recipe.picture,
+            ingredients: await Promise.all(
+                recipe.ingredients.map(async (ingredient) => ({
+                    ingredient: await getIngredientById(ingredient.id, session),
+                    amount: ingredient.amount,
+                    unit: ingredient.unit,
+                }))
+            ),
+            publicResources: recipe.publicResources,
+        }))
+    )
+
+    return recipes
+}
+
+export const deleteRecipe = async (id: UUID, session: Session) => {
+    const filePath = join(
+        process.cwd(),
+        'src',
+        'data',
+        'users',
+        session.login,
+        'recipes.json'
+    )
+    const recipes: Recipe[] = await getFromFile(filePath)
+    const newRecipes = recipes.filter((recipe) => recipe.id !== id)
+    await setToFile(filePath, newRecipes)
+}
+
+export const patchRecipe = async (
+    {
+        id,
+        name,
+        description,
+        instructions,
+        picture,
+        ingredients,
+        cost,
+        publicResources,
+    }: PatchRecipeDTO,
+    session: Session
+) => {
+    const filePath = join(
+        process.cwd(),
+        'src',
+        'data',
+        'users',
+        session.login,
+        'recipes.json'
+    )
+
+    const recipes: Recipe[] = await getCompressedRecipes(session)
+    const unchangedRecipes = recipes.filter((recipe) => recipe.id !== id)
+    const toPatchRecipe = recipes.find((recipe) => recipe.id === id)
+    if (!toPatchRecipe) {
+        throw new Error(`Recipe with id: ${id} does not exist`)
+    }
+
+    toPatchRecipe.name = name ?? toPatchRecipe.name
+    toPatchRecipe.description = description ?? toPatchRecipe.description
+    toPatchRecipe.instructions = instructions ?? toPatchRecipe.instructions
+    toPatchRecipe.picture = picture ?? toPatchRecipe.picture
+    toPatchRecipe.ingredients = ingredients ?? toPatchRecipe.ingredients
+    toPatchRecipe.cost = cost ?? toPatchRecipe.cost
+    toPatchRecipe.publicResources = publicResources ?? toPatchRecipe.publicResources
+
+    const newRecipes = [...unchangedRecipes, toPatchRecipe]
+    await setToFile(filePath, newRecipes)
+}
