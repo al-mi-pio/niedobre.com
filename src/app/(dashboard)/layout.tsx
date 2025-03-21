66056100664a2ee08afb361dc432522f82d87291
ThePanToster <alcrafter@o2.pl>
2025-03-21 01:48:31 +0100

    Add toast messages

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,6 +4,7 @@ import { AppProvider } from '@toolpad/core/AppProvider'
 import { ReactNode } from 'react'
 import { MenuBar } from '@/app/(dashboard)/MenuBar'
 import { appName } from '@/constants/general'
+import { NotificationsProvider } from '@toolpad/core'
 
 export const metadata: Metadata = {
     title: `Panel - ${appName}`,
@@ -16,15 +17,23 @@ const DashboardLayout = ({
     children: ReactNode
 }>) => (
     <AppProvider>
-        <div
-            style={{
-                maxHeight: '100vh',
-                minHeight: '100vh',
+        <NotificationsProvider
+            slotProps={{
+                snackbar: {
+                    anchorOrigin: { vertical: 'bottom', horizontal: 'center' },
+                },
             }}
         >
-            <MenuBar />
-            {children}
-        </div>
+            <div
+                style={{
+                    maxHeight: '100vh',
+                    minHeight: '100vh',
+                }}
+            >
+                <MenuBar />
+                {children}
+            </div>
+        </NotificationsProvider>
     </AppProvider>
 )
 
