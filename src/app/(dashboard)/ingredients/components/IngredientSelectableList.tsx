11dcd21c9895c40ce12848511c76039d52a362d0
ThePanToster <alcrafter@o2.pl>
2025-03-26 18:33:23 +0100

    Add filtering

--- src/app/(dashboard)/ingredients/components/IngredientSelectableList.tsx
+++ src/app/(dashboard)/ingredients/components/IngredientSelectableList.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
+import { SearchField } from '@/components/SearchField'
 import {
-    Avatar,
-    List,
-    ListItemButton,
     ListItemAvatar,
     ListItemText,
+    List,
+    Stack,
     Paper,
+    Avatar,
+    ListItemButton,
 } from '@mui/material'
 import EggIcon from '@mui/icons-material/Egg'
 import AddIcon from '@mui/icons-material/Add'
@@ -23,38 +26,51 @@ export const IngredientSelectableList = ({
     onClick,
     selectedIngredientId,
     onNew,
-}: Props) => (
-    <Paper variant="outlined">
-        <List
-            sx={{
-                height: '68vh',
-                whiteSpace: 'nowrap',
-                overflowY: 'scroll',
-                width: '26em',
-            }}
-        >
-            {ingredients.map((ingredient) => (
-                <ListItemButton
-                    key={ingredient.id}
-                    onClick={() => onClick(ingredient)}
-                    selected={selectedIngredientId === ingredient.id}
-                >
-                    <ListItemAvatar>
-                        <Avatar>
-                            <EggIcon />
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={ingredient.name} />
-                </ListItemButton>
-            ))}
-        </List>
-        <ListItemButton onClick={onNew}>
-            <ListItemAvatar>
-                <Avatar>
-                    <AddIcon />
-                </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Dodaj nowy składnik" />
-        </ListItemButton>
-    </Paper>
-)
+}: Props) => {
+    const [filterInput, setFilterInput] = useState<string>('')
+    const filteredIngredients = ingredients.filter((ingredient) =>
+        ingredient.name.includes(filterInput)
+    )
+    return (
+        <Paper variant="outlined">
+            <Stack sx={{ padding: '0.5em 5em' }}>
+                <SearchField
+                    value={filterInput}
+                    onChange={(e) => setFilterInput(() => e.target.value)}
+                />
+            </Stack>
+
+            <List
+                sx={{
+                    height: '68vh',
+                    whiteSpace: 'nowrap',
+                    overflowY: 'scroll',
+                    width: '26em',
+                }}
+            >
+                {filteredIngredients.map((ingredient) => (
+                    <ListItemButton
+                        key={ingredient.id}
+                        onClick={() => onClick(ingredient)}
+                        selected={selectedIngredientId === ingredient.id}
+                    >
+                        <ListItemAvatar>
+                            <Avatar>
+                                <EggIcon />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={ingredient.name} />
+                    </ListItemButton>
+                ))}
+            </List>
+            <ListItemButton onClick={onNew}>
+                <ListItemAvatar>
+                    <Avatar>
+                        <AddIcon />
+                    </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Dodaj nowy składnik" />
+            </ListItemButton>
+        </Paper>
+    )
+}
