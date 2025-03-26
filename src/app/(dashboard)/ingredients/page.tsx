4ade1da404cdb4831c32f4d0f23d29ad4f27106a
ThePanToster <alcrafter@o2.pl>
2025-03-26 01:59:14 +0100

    Add basic login for the page

--- src/app/(dashboard)/ingredients/page.tsx
+++ src/app/(dashboard)/ingredients/page.tsx
@@ -1,11 +1,24 @@
 'use client'
 
+import AddIcon from '@mui/icons-material/Add'
+import CloseIcon from '@mui/icons-material/Close'
+import EggIcon from '@mui/icons-material/Egg'
+import DeleteIcon from '@mui/icons-material/Delete'
 import { Grid } from '@mui/system'
 import { useNotifications } from '@toolpad/core'
 import { ChangeEvent, useEffect, useState } from 'react'
 import { Spinner } from '@/components/Spinner'
 import {
     Avatar,
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    IconButton,
+    InputAdornment,
     List,
     ListItemAvatar,
     ListItemButton,
@@ -14,22 +27,34 @@ import {
     Paper,
     Stack,
     TextField,
+    Typography,
 } from '@mui/material'
-import EggIcon from '@mui/icons-material/Egg'
 import { getSession } from '@/utils/session'
 import { autoHideDuration } from '@/constants/general'
 import { Ingredient, MassUnit, Unit, VolumeUnit } from '@/types/Ingredient'
-import { getIngredients } from '@/services/ingredientService'
-import { ingredientToForm } from '@/utils/ingredients'
-import { units } from '@/constants/ingredients'
+import {
+    createIngredient,
+    deleteIngredient,
+    getIngredients,
+    patchIngredient,
+} from '@/services/ingredientService'
+import {
+    formToCreateIngredientDTO,
+    formToPatchIngredientDTO,
+    ingredientToForm,
+} from '@/utils/ingredients'
+import { massUnits, units, volumeUnits } from '@/constants/ingredients'
+import { UUID } from 'crypto'
 
 export type IngredientForm = {
-    id?: string
+    id?: UUID
     name: string
     amount?: number
     oppositeAmount?: number
+    costAmount?: number
     cost?: number
     kcal?: number
+    isNew?: boolean
 } & (
     | {
           unit?: MassUnit
@@ -45,27 +70,108 @@ export type IngredientForm = {
       }
 )
 
+const emptyForm: IngredientForm = { name: '' }
+const newForm: IngredientForm = { name: '', isNew: true }
+
+const validateForm = (form: IngredientForm) => {
+    return !!form.name && !!form.unit
+}
+
+const selectOutOfScope = (form: IngredientForm) => {
+    return (
+        (massUnits.includes(form.unit as MassUnit) &&
+            massUnits.includes(form.oppositeUnit as MassUnit)) ||
+        (volumeUnits.includes(form.unit as VolumeUnit) &&
+            volumeUnits.includes(form.oppositeUnit as VolumeUnit))
+    )
+}
+
 const Ingredients = () => {
     const toast = useNotifications()
     const [ingredients, setIngredients] = useState<Ingredient[]>([])
     const [selectedIngredient, setSelectedIngredient] = useState<Ingredient | null>(null)
-    const [ingredientForm, setIngredientForm] = useState<IngredientForm>({ name: '' })
+    const [ingredientForm, setIngredientForm] = useState<IngredientForm>(emptyForm)
     const [loading, setLoading] = useState(true)
+    const [saving, setSaving] = useState(false)
+    const oppositeUnits = massUnits.includes(ingredientForm.unit as MassUnit)
+        ? volumeUnits
+        : massUnits
+
+    const [modalOpen, setModalOpen] = useState(false)
+
+    const handleModalOpen = () => {
+        setModalOpen(true)
+    }
+
+    const handleModalClose = () => {
+        setModalOpen(false)
+    }
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setIngredientForm((prevForm) => ({
             ...prevForm,
+            oppositeUnit: selectOutOfScope(prevForm) ? '' : prevForm.oppositeUnit,
+            costAmount: e.target.name === 'amount' ? e.target.value : prevForm.costAmount,
             [e.target.name]: e.target.value,
         }))
     }
 
-    useEffect(() => {
-        const handleIngredientGet = async () => {
-            const newIngredients = await getIngredients(getSession())
-            setIngredients(() => newIngredients)
-        }
+    const handleSave = async () => {
+        setSaving(true)
+        try {
+            const session = getSession()
+            if (selectedIngredient)
+                await patchIngredient(formToPatchIngredientDTO(ingredientForm), session)
+            else
+                await createIngredient(formToCreateIngredientDTO(ingredientForm), session)
 
-        handleIngredientGet()
+            setSaving(false)
+            setLoading(true)
+            loadIngredients()
+            // TODO: Add errors handling
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (e) {
+            toast.show(`Problem`, {
+                severity: 'error',
+                autoHideDuration,
+            })
+        }
+        setSaving(false)
+    }
+
+    const handleDelete = async () => {
+        setModalOpen(false)
+        setLoading(true)
+        try {
+            const session = getSession()
+            if (selectedIngredient?.id)
+                await deleteIngredient(selectedIngredient?.id, session)
+
+            loadIngredients()
+            // TODO: Add errors handling
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (e) {
+            toast.show(`Problem`, {
+                severity: 'error',
+                autoHideDuration,
+            })
+            setLoading(false)
+        }
+    }
+
+    const handleOnClose = () => {
+        setSelectedIngredient(null)
+        setIngredientForm(emptyForm)
+    }
+
+    const handleOnNew = () => {
+        setSelectedIngredient(null)
+        setIngredientForm(newForm)
+    }
+
+    const loadIngredients = () => {
+        getIngredients(getSession())
+            .then((newIngredients) => setIngredients(() => newIngredients))
             .catch((e) =>
                 toast.show(`Problem z załadowaniem składników: ${e.message}`, {
                     severity: 'error',
@@ -73,7 +179,12 @@ const Ingredients = () => {
                 })
             )
             .finally(() => setLoading(false))
-    }, [toast])
+    }
+
+    useEffect(() => {
+        loadIngredients()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     useEffect(() => {
         if (selectedIngredient)
@@ -81,11 +192,16 @@ const Ingredients = () => {
     }, [selectedIngredient])
 
     return (
-        <Grid container spacing={2} margin={2}>
+        <Grid
+            container
+            spacing={2}
+            margin={2}
+            style={{ justifyContent: 'center', gap: '50px' }}
+        >
             <Grid
                 container
                 spacing={2}
-                size={8}
+                size={2}
                 style={{
                     maxHeight: '86vh',
                     overflow: 'auto',
@@ -100,6 +216,7 @@ const Ingredients = () => {
                             <ListItemButton
                                 key={ingredient.id}
                                 onClick={() => setSelectedIngredient(ingredient)}
+                                selected={selectedIngredient?.id === ingredient.id}
                             >
                                 <ListItemAvatar>
                                     <Avatar>
@@ -109,34 +226,71 @@ const Ingredients = () => {
                                 <ListItemText primary={ingredient.name} />
                             </ListItemButton>
                         ))}
+                        <ListItemButton onClick={handleOnNew}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <AddIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Dodaj nowy składnik" />
+                        </ListItemButton>
                     </List>
                 )}
             </Grid>
 
             <Grid style={{ minHeight: '86vh' }} size={4}>
-                {!!selectedIngredient && (
+                {(!!selectedIngredient || ingredientForm.isNew) && (
                     <Paper sx={{ height: '100%' }}>
                         <Stack
                             direction="column"
                             padding={4}
                             spacing={4}
+                            useFlexGap
                             sx={{
                                 height: '100%',
                             }}
                         >
+                            <Stack
+                                direction="row"
+                                sx={{
+                                    justifyContent: 'space-between',
+                                }}
+                            >
+                                <Typography variant="h4">
+                                    {selectedIngredient ? 'Edytuj' : 'Dodaj'}
+                                </Typography>
+                                <Box>
+                                    {selectedIngredient && (
+                                        <IconButton
+                                            style={{
+                                                marginRight: '10px',
+                                            }}
+                                            onClick={handleModalOpen}
+                                        >
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    )}
+
+                                    <IconButton onClick={handleOnClose}>
+                                        <CloseIcon />
+                                    </IconButton>
+                                </Box>
+                            </Stack>
                             <TextField
                                 required
                                 label="Nazwa"
-                                value={ingredientForm.name}
+                                value={ingredientForm.name ?? ''}
                                 name="name"
                                 onChange={handleInputChange}
                             />
 
                             <TextField
                                 select
+                                required
                                 label="Jednostka bazowa"
-                                value={ingredientForm.unit}
+                                value={ingredientForm.unit ?? ''}
                                 name="unit"
+                                sx={{ width: '22ch' }}
                                 onChange={handleInputChange}
                             >
                                 {units.map((unit) => (
@@ -146,19 +300,150 @@ const Ingredients = () => {
                                 ))}
                             </TextField>
 
+                            {ingredientForm.unit && ingredientForm.unit !== 'szt.' && (
+                                <Stack
+                                    direction="row"
+                                    spacing={2}
+                                    sx={{
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <TextField
+                                        required
+                                        value={ingredientForm.amount ?? ''}
+                                        name="amount"
+                                        onChange={handleInputChange}
+                                        sx={{ width: '12ch' }}
+                                        slotProps={{
+                                            input: {
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        {ingredientForm.unit}
+                                                    </InputAdornment>
+                                                ),
+                                            },
+                                        }}
+                                    />
+
+                                    <Typography variant="body1" color="textPrimary">
+                                        {'jest równoważne'}
+                                    </Typography>
+
+                                    <Box>
+                                        <TextField
+                                            required
+                                            value={ingredientForm.oppositeAmount ?? ''}
+                                            name="oppositeAmount"
+                                            onChange={handleInputChange}
+                                            sx={{ width: '8ch' }}
+                                        />
+
+                                        <TextField
+                                            select
+                                            required
+                                            value={
+                                                ingredientForm.oppositeUnit &&
+                                                !selectOutOfScope(ingredientForm)
+                                                    ? ingredientForm.oppositeUnit
+                                                    : ''
+                                            }
+                                            name="oppositeUnit"
+                                            sx={{ width: 'fit-content' }}
+                                            onChange={handleInputChange}
+                                        >
+                                            {oppositeUnits.map((unit) => (
+                                                <MenuItem key={unit} value={unit}>
+                                                    {unit}
+                                                </MenuItem>
+                                            ))}
+                                        </TextField>
+                                    </Box>
+                                </Stack>
+                            )}
+
                             <TextField
                                 label="Wartość kaloryczna"
-                                value={ingredientForm.kcal}
+                                value={ingredientForm.kcal ?? ''}
                                 name="kcal"
                                 onChange={handleInputChange}
-                                //endAdornment={<InputAdornment position="end">kg</InputAdornment>}
                             />
+
+                            <Stack
+                                direction="row"
+                                spacing={2}
+                                sx={{
+                                    alignItems: 'center',
+                                }}
+                            >
+                                <TextField
+                                    required
+                                    value={ingredientForm.costAmount ?? ''}
+                                    name="costAmount"
+                                    onChange={handleInputChange}
+                                    sx={{ width: '12ch' }}
+                                    slotProps={{
+                                        input: {
+                                            endAdornment: (
+                                                <InputAdornment position="end">
+                                                    {ingredientForm.unit}
+                                                </InputAdornment>
+                                            ),
+                                        },
+                                    }}
+                                />
+
+                                <Typography variant="body1" color="textPrimary">
+                                    {'kosztuje'}
+                                </Typography>
+
+                                <TextField
+                                    required
+                                    value={ingredientForm.cost ?? ''}
+                                    name="cost"
+                                    onChange={handleInputChange}
+                                    sx={{ width: '12ch' }}
+                                    slotProps={{
+                                        input: {
+                                            endAdornment: (
+                                                <InputAdornment position="end">
+                                                    {'zł'}
+                                                </InputAdornment>
+                                            ),
+                                        },
+                                    }}
+                                />
+                            </Stack>
+                            <Button
+                                onClick={handleSave}
+                                disabled={!validateForm(ingredientForm)}
+                                loading={saving}
+                                variant="contained"
+                                sx={{ marginTop: 'auto' }}
+                            >
+                                {'Zapisz'}
+                            </Button>
                         </Stack>
                     </Paper>
                 )}
             </Grid>
+            <Dialog open={modalOpen} onClose={handleModalClose}>
+                <DialogTitle>{`Usunąć ${selectedIngredient?.name}?`}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {`Czy aby napewno chcesz usunąć składnik o nazwie ${selectedIngredient?.name}?`}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleModalClose}>{'Nie'}</Button>
+                    <Button onClick={handleDelete} autoFocus>
+                        {'Tak'}
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Grid>
     )
 }
 
 export default Ingredients
+
+// TODO: try to find a fix for "Blocked aria-hidden on an element because its descendant retained focus."
