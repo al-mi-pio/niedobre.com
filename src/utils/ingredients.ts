a16f5cb97814cdf45a79da45eaa7318d520a0140
ThePanToster <alcrafter@o2.pl>
2025-03-26 01:51:22 +0100

    Add skeletons for form data converters

--- src/utils/ingredients.ts
+++ src/utils/ingredients.ts
@@ -1,10 +1,29 @@
-import { Ingredient } from '@/types/Ingredient'
+import { CreateIngredientDTO, Ingredient, PatchIngredientDTO } from '@/types/Ingredient'
+import { IngredientForm } from '@/app/(dashboard)/ingredients/page'
 
 // TODO
-export const ingredientToForm = (ingredient: Ingredient) => {
+export const ingredientToForm = (ingredient: Ingredient): IngredientForm => {
     return {
         id: ingredient.id,
         name: ingredient.name,
         kcal: ingredient.kcal,
+    }
+}
+
+// TODO
+export const formToCreateIngredientDTO = (form: IngredientForm): CreateIngredientDTO => {
+    return {
+        name: form.name,
+        type: 'mass',
+    }
+}
+
+// TODO
+export const formToPatchIngredientDTO = (form: IngredientForm): PatchIngredientDTO => {
+    if (!form.id) {
+        throw new Error('id')
+    }
+    return {
+        id: form.id,
     }
 }
