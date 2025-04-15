61e36911443c6e14183256b0357b81ece9e23de3
ThePanToster <alcrafter@o2.pl>
2025-04-16 01:31:46 +0200

    Extract missingIngredientAndNutritionalValues() util

--- src/app/(dashboard)/utils.ts
+++ src/app/(dashboard)/utils.ts
@@ -1,3 +1,4 @@
+import { IngredientAmount, MissingValues } from '@/types/Ingredient'
 import { GetRecipeDTO } from '@/types/Recipe'
 
 export const createSelectedRecipeStructure = (recipes: GetRecipeDTO[]) =>
@@ -20,3 +21,35 @@ export const createSelectedRecipeStructure = (recipes: GetRecipeDTO[]) =>
               }
           )
         : {}
+
+export const missingIngredientAndNutritionalValues = (
+    ingredients: IngredientAmount[]
+): MissingValues => {
+    return {
+        cost: ingredients.reduce(
+            (prev, curr) => (prev ? true : !curr.ingredient.cost && !!curr.amount),
+            false
+        ),
+        kcal: ingredients.reduce(
+            (prev, curr) => (prev ? true : !curr.ingredient.kcal && !!curr.amount),
+            false
+        ),
+        protein: ingredients.reduce(
+            (prev, curr) => (prev ? true : !curr.ingredient.protein && !!curr.amount),
+            false
+        ),
+        fat: ingredients.reduce(
+            (prev, curr) => (prev ? true : !curr.ingredient.fat && !!curr.amount),
+            false
+        ),
+        carbohydrates: ingredients.reduce(
+            (prev, curr) =>
+                prev ? true : !curr.ingredient.carbohydrates && !!curr.amount,
+            false
+        ),
+        salt: ingredients.reduce(
+            (prev, curr) => (prev ? true : !curr.ingredient.salt && !!curr.amount),
+            false
+        ),
+    }
+}
