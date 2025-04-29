33f4d390b7ff7733dda215e87950e167bf2857ce
piotr102k <piotrswiszcz2002@wp.pl>
2025-04-30 01:21:53 +0200

    Add rate limiter get from env

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -1,5 +1,15 @@
 import { UUID } from 'crypto'
+import get from '@/utils/config'
+import { RateLimiterMemory } from 'rate-limiter-flexible'
 
+export const authRateLimiter = new RateLimiterMemory({
+    points: parseInt(get.authRateLimiterPoints()),
+    duration: parseInt(get.authRateLimiterDuration()),
+})
+export const rateLimiter = new RateLimiterMemory({
+    points: parseInt(get.rateLimiterPoints()),
+    duration: parseInt(get.rateLimiterDuration()),
+})
 export const appName = 'nieDOBRE.com'
 export const autoHideDuration = 6000
 export const emptyUUID: UUID = '0-0-0-0-0-0'
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,6 +3,10 @@ const get = {
     keyLength: () => parseInt(process.env.KEY_LENGTH ?? '32'),
     hashAlgorithm: () => process.env.HASH_ALGORITHM ?? 'sha256',
     mailPassword: () => process.env.MAIL_PASSWORD ?? '',
+    authRateLimiterPoints: () => process.env.AUTH_RATE_LIMIT_POINTS ?? '10',
+    authRateLimiterDuration: () => process.env.AUTH_RATE_LIMIT_DURATION ?? '60',
+    rateLimiterPoints: () => process.env.RATE_LIMIT_POINTS ?? '200',
+    rateLimiterDuration: () => process.env.RATE_LIMIT_DURATION ?? '60',
 }
 
 export default get
